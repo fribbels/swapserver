8c93629428b5d7965cf0423b09cf7f117a1bb17d
Ivan Chen <ivanchenschool@gmail.com>
2016-12-08 00:08:07 -0500

    printing debug2

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ def index():
         'posts': [] 
     }
 
+    print "get post"
+    sys.stdout.flush()
+    
     for post in Post.query.all():
         data = {}
         data['title'] = post.title
@@ -45,7 +48,9 @@ def index():
 @app.route('/post/', methods = ['POST'])
 def create_post():
     content = request.json
+    print "new post"
     print content['title']
+    sys.stdout.flush()
     #if not request.json or not 'title' in request.json:
     #    abort(400)
     post = Post(title=content['title'], desc=content['desc'])
