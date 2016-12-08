756383b5b56f8f73fc4731db19fc32b5c2d207c2
Ivan Chen <ivanchenschool@gmail.com>
2016-12-07 23:01:38 -0500

    api done

--- app.py
+++ app.py
@@ -1,16 +1,67 @@
 import os
-from flask import Flask
+import json
+from flask import Flask, jsonify, abort, request
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)    
 
-@app.route("/")
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    desc = db.Column(db.Text)
 
-def hello():
-    return "Hello from Python!"
+    #def __init__(self, title, desc):
+    #    self.title = title
+    #    self.desc = desc
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #def __repr__(self):
+    #    return '<Post %r>' % self.title
+    def __init__(self, title, desc):
+        self.title = title
+        self.desc = desc
+
+db.create_all()
+
+@app.route('/get/', methods = ['GET'])
+def index():
+    result = { 
+        'posts': [] 
+    }
+
+    for post in Post.query.all():
+        data = {}
+        data['title'] = post.title
+        data['desc'] = post.desc
+        result['posts'].append(data)
+
+    return jsonify(result)
+    #return jsonify({'posts': Post.query.all()})
+
+
+@app.route('/post/', methods = ['POST'])
+def create_dev():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    post = Post(request.json.title, request.json.get('desc', ''))
+    db.session.add(post)
+    db.session.commit()
+    return jsonify( { 'post': post } ), 201
+
+
+if __name__ == '__main__':
+    app.run()
+#########
+
+
+#@app.route("/")
+
+#def hello():
+#    return "Hello from Python!"
+
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 5000))
+#    app.run(host='0.0.0.0', port=port)
---
+++ model.py
@@ -0,0 +1,38 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    desc = db.Column(db.Text)
+
+    #def __init__(self, title, desc):
+    #    self.title = title
+    #    self.desc = desc
+
+    #def __repr__(self):
+    #    return '<Post %r>' % self.title
+    def __init__(self, title, desc):
+        self.title = title
+        self.desc = desc
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
