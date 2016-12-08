dfcafcdf3c48cc61de962876abb3c0553d75ab5e
Ivan Chen <ivanchenschool@gmail.com>
2016-12-07 20:19:53 -0500

    uniqueness error

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ if __name__ == '__main__':
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    id = db.Column(db.Integer, primary_key=False)
+    username = db.Column(db.String(80), unique=False)
+    email = db.Column(db.String(120), unique=False)
 
     def __init__(self, username, email):
         self.username = username
