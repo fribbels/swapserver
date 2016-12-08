338b5cfec4578e8ce40721b2fa91f2ec57d1e28f
Ivan Chen <ivanchenschool@gmail.com>
2016-12-07 21:17:53 -0500

    readded hello world for testing

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+@app.route("/")
+def hello():
+        return "Hello from Python!"
