f6f034a1a62584d4fccc275346891db9fa56d2c1
Ivan Chen <ivanchenschool@gmail.com>
2016-12-07 21:42:32 -0500

    yolo swag

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,5 +7,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 @app.route("/")
+
 def hello():
-        return "Hello from Python!"
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
